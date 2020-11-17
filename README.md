c0b622a30261dfacade192fbfc56e71723b66e56
Frenz86 <frenzy86@gmail.com>
2020-11-17 16:43:45 +0100

    weerw

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Iris_Streamlit
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/Frenzy86/Iris_Streamlit/)
--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import joblib
 from PIL import Image
 
+image = Image.open('iris.png')
+st.image(image, caption='',use_column_width=True)
+
 #Loading Our final trained Knn model 
 model= open("Knn_Classifier.pkl", "rb")
 knn_clf=joblib.load(model)
@@ -28,15 +31,22 @@ values=[]
 #Display
 for parameter, parameter_df in zip(parameter_list, parameter_default_values):
 	
-	values= st.sidebar.slider(label=parameter, key=parameter,value=float(parameter_df), min_value=0.0, max_value=8.0, step=0.1)
+	values= st.sidebar.slider(label=parameter, 
+                               key=parameter,
+                               value=float(parameter_df),
+                               min_value=0.0, 
+                               max_value=8.0, 
+                               step=0.1)
+ 
 	parameter_input_values.append(values)
 	
 input_variables=pd.DataFrame([parameter_input_values],columns=parameter_list,dtype=float)
-st.write('\n\n')
 
-if st.button("Click Here to Classify"):
+if st.button("Risultato Classificazione"):
 	prediction = knn_clf.predict(input_variables)
-	st.image(setosa) if prediction == 0 else st.image(versicolor)  if prediction == 1 else st.image(virginica) 
-	
-
-
+	st.image(setosa) if prediction == 0 else \
+    st.image(versicolor) if prediction == 1 else \
+    st.image(virginica)
+	st.write('La classe di appartenenza è SETOSA') if prediction == 0 else \
+    st.write('La classe di appartenenza è VERSICOLOR')  if prediction == 1 else \
+    st.write('La classe di appartenenza è VIRGINICA')
